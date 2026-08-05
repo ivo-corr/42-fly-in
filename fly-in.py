5984edefb0fad0686a71cd973088406ea82937f3
ivo <icorrale@student.42heilbronn.de>
2026-08-05 14:35:06 +0200

    parsing

--- fly-in.py
+++ fly-in.py
@@ -1,13 +1,77 @@
+from enum import Enum
+
+
+class Color(Enum):
+    NONE = 0
+    GREEN = 1
+    RED = 2
+    BLUE = 3
+
+
+class ZoneType(Enum):
+    NORMAL = 0
+    BLOCKED = 1
+    RESTRICTED = 2
+    PRIORITY = 3
+
+
+class Map():
+    class Zone():
+        class Connection():
+            def __init__(self, orig: "Map.Zone", dest: "Map.Zone",
+                         max_capacity: int = -1):
+                self.orig = orig
+                self.dest = dest
+                self.capacity = max_capacity
+
+        def __init__(self, name: str, coords: tuple[str, str] | list[str],
+                     color: str = "NONE"):
+            self.name: str = name
+            self.coords: tuple[int, int] | list[int] = [int(x) for x in coords]
+            self.type = ZoneType.NORMAL
+            self.color = [
+                c for c in Color if str(c) == "Color." + color.upper()][0]
+            self.connections: list[Map.Zone.Connection] = []
+
+    def __init__(self, pconfig: str):
+        breakpoint()
+        self._zones: list[Map.Zone] = []
+        for c in pconfig:
+            if ('hub' in c[0]):
+                self._zones.append(Map.Zone(c[1].split(" ")[0],
+                                            c[1].split(" ")[1:3],
+                                            color=tmp if
+                                            (tmp := c[1].split("[")[1].split(
+                                                '=')[1][:-1]).upper() in
+                                            Color.__members__
+                                            else "none"))
+            if (c[0].lower() == "connection"):
+                print(f"Found connection {c}")
+
+    def get_zones(self):
+        return self._zones
+
+
 def parse_config(file: str):
-    result: list[list[str]] = []
+    result: list[list[str] | list[list[list[str]]]] = []
     splat: list[str] = file.split("\n")
     splat = [s for s in splat if not s.startswith("#")]
     result.extend(
         [[s[0], s[1]] for s in
          [ss.split(": ") for ss in splat if len(ss.split(": ")) == 2]])
+    # for r in result:
+    #     r[1] = r[1].split(" ", 1)
+    #     if len(r) >= 2 and "=" in r[1]:
+    #         result[result.index(r)] = [
+    #             r[1].split("[")[0],
+    #             [x[:-1] if x[-1] == ']' else x for x in
+    #              r[1].split("[")[1].split("=")]]
     return (result)
 
 
 if __name__ == "__main__":
     with open("maps/easy/01_linear_path.txt") as file:
-        print(parse_config(file.read()))
+        pconfig = parse_config(file.read())
+    # [print(c) for c in pconfig]
+    m: Map = Map(pconfig)
+    print(m.get_zones())
